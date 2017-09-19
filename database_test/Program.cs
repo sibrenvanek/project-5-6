52838972c454291f9a6b24f12ddc6735c9f5f43b
ErikvBerk <erik.van.berk@gmail.com>
2017-09-19 14:16:36 +0200

    db 1

--- database_test/Program.cs
+++ database_test/Program.cs
@@ -1,24 +1,36 @@
-﻿using System;
+using System;
+using System.Data;
 
-using database_test.Models;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
-namespace database_test
+public class Tutorial2
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        string connStr = "server=145.24.198.66;user=guest;database=prj_56_db;port=3306;password=guest;SslMode=none";
+        MySqlConnection conn = new MySqlConnection(connStr);
+        try
         {
-            inputTest("Sibren2");
-            Console.WriteLine("Done");
-        }
-        static void inputTest(string name)
-        {
-            using (var db = new testContext())
+            Console.WriteLine("Connecting to MySQL...");
+            conn.Open();
+
+            string sql = "SELECT Name, HeadOfState FROM Country WHERE Continent='Oceania'";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
             {
-                Test t = new Test { Name = name };
-                db.Add(t);
-                db.SaveChanges();
+                Console.WriteLine(rdr[0]+" -- "+rdr[1]);
             }
+            rdr.Close();
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+
+        conn.Close();
+        Console.WriteLine("Done.");
     }
 }
