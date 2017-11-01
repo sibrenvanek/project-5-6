a0ffbc37c6654e08d58e792b47be6ff80ce034ab
sibrenvanek <0924655@hr.nl>
2017-11-01 09:40:24 +0100

    edit webshop

---
+++ webshop/Pages/Login.cshtml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webshop.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnPost()
+        {
+            Console.WriteLine("post");
+            RedirectToAction("Index");
+        }
+    }
+}
--- webshop/Startup.cs
+++ webshop/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -33,7 +33,7 @@ namespace webshop
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
             }
 
             app.UseStaticFiles();
@@ -42,7 +42,7 @@ namespace webshop
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
