0c915f380a2d4d986ac4bf855e764f30bd83b21a
ErikvBerk <erik.van.berk@gmail.com>
2017-11-14 00:20:50 +0100

    connecting to db 0.1

---
+++ prj56/prj56/Controllers/DbController.cs
@@ -0,0 +1,33 @@
+﻿using prj56.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prj56.Controllers
+{
+    public class db_Entities : Controller
+    {
+        db_Entities DB = new db_Entities();
+        
+        // GET: Db
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public ActionResult Cases()
+        {
+            List<Case> ListCases = new List<Case>();
+            ListCases.Add(new Case { CaseId = 1 });
+
+
+
+            return View(ListCases);
+        }
+
+    }
+}
