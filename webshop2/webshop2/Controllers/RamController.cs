e864b8c36fc3a238c4719bbd225a731cf6e55ecb
ErikvBerk <erik.van.berk@gmail.com>
2017-12-03 20:23:43 +0100

    Ram image view ( needs correct path)

--- webshop2/webshop2/Controllers/RamController.cs
+++ webshop2/webshop2/Controllers/RamController.cs
@@ -10,24 +10,27 @@ namespace webshop2.Controllers
     public class RamController : Controller
     {
         // GET: Image
+        [HttpGet]
         public ActionResult View(int id)
         {
             ram ramimage = new ram();
 
             using (new_testEntities3 db = new new_testEntities3())
             {
-                db.ram.Add(new ram {
-                    Brand ="testbrand",
-                    CompositionOfMemory ="wut",
-                    MemoryType ="testtype",
-                    Clockspeed=1,
-                    CASlatency=1,
-                    Voltage=1,
-                    MemoryModuleConnection=50,
-                    MemorySuitableFor="datatest",
-                    ImagePath = "C:/Users/erikv/Desktop/PRJ 56/project-5-6 - kopie/webshop2/webshop2/Content/ram1.jpg" });
-                 
-                db.SaveChanges();
+                //db.ram.Add(new ram
+                //{
+                //    Brand = "testbrand",
+                //    CompositionOfMemory = "wut",
+                //    MemoryType = "testtype",
+                //    Clockspeed = 1,
+                //    CASlatency = 1,
+                //    Voltage = 1,
+                //    MemoryModuleConnection = 50,
+                //    MemorySuitableFor = "datatest",
+                //    ImagePath = "C:/Users/Erik/Desktop/Prj5-6/project-5-6 - Copy/webshop2/webshop2/Contentram1.jpg"
+                //});
+                
+                //db.SaveChanges();
 
                 ramimage = db.ram.Where(x => x.RamId == id).FirstOrDefault();
             }
