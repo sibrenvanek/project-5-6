db2bc90339b08c8e4cd099aabf531e12b705b95e
sibrenvanek <0924655@hr.nl>
2017-12-15 10:36:25 +0100

    working filter(sortof)

--- webshop2/webshop2/Controllers/RamController.cs
+++ webshop2/webshop2/Controllers/RamController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Services;
 using webshop2.Models;
 
 namespace webshop2.Controllers
@@ -13,10 +14,20 @@ namespace webshop2.Controllers
         {
             using (new_testEntities db = new new_testEntities())
             {
-                return View(db.ram.ToList());
+                return FilterRam(0, 100);
+                //return View(db.ram.ToList());
             }
                 
         }
+        [WebMethod]
+        public ActionResult FilterRam(int min, int max)
+        {
+            using(new_testEntities db = new new_testEntities())
+            {
+                return View(db.ram.Where(r => r.Price >= min && r.Price <= max).ToList());
+            }
+        }
+
         // GET: Image
         [HttpGet]
         public ActionResult View(int? id)
