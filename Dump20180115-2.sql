d589fb56235849f185eac3cd9114b9f173b754ea
ErikvBerk <erik.van.berk@gmail.com>
2018-01-15 12:57:14 +0100

    db 1.4

--- Dump20180115-2.sql
+++ Dump20180115-2.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `new_test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `new_test`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: new_test
@@ -551,4 +553,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-01-15 12:47:26
+-- Dump completed on 2018-01-15 12:56:51
